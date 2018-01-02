063cb3a9bf6c62cba0741707aa41e79430edfae9
Abby Christens <achristens@gmail.com>
2018-01-02 09:54:13 -0500

    Adding rooms and descriptions to adventure game

---
+++ adventure.py
@@ -0,0 +1,12 @@
+# estbalish directions for user to move
+# track how far the user has moved
+# set limits for how far a user can move (ie establish walls)
+
+rooms = {
+    'library': 'This is the panultimate room for the socially uninclined. Filled with wall-to-ceiling books, loads of natural light, hot tea, and endless cushions, you will never want to leave.',
+    'dining room': 'Come for the food, stay for the people. Furnished with a live-edge dining table and a sizeable wine collection, you will immediately feel at home and ready to eat.',
+    'kitchen': 'Snacks and inspiration galore. The big marble island lets you graze, prep, or pour over an Ottolenghi recipe.',
+    'living room': 'Welcome to my home! Have a sit here on the sectional and play with the pupper before you move on. Maybe a catch an episode of Gilmore Girls while you are at it.'
+}
+
+print(rooms['library'])
